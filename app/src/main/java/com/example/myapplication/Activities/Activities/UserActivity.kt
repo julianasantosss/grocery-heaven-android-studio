6e3c998ed718ec0b459e22dedf4ec1801a9a5318
julianasantosss <corazondemayr@gmail.com>
2025-05-21 00:27:11 -0500

    Restore previous stable state before broken changes

---
+++ app/src/main/java/com/example/myapplication/Activities/Activities/UserActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication.Activities.Activities
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.Activities.data.database.AppDatabase
+import com.example.myapplication.Activities.data.model.User
+import com.example.myapplication.Activities.data.repository.UserRepository
+import com.example.myapplication.Activities.viewmodel.ProductViewModel
+import com.example.myapplication.Activities.viewmodel.UserViewModel
+import com.example.myapplication.Activities.viewmodel.UserViewModelFactory
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivitySignUpBinding
+
+class UserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var viewModel: UserViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dao = AppDatabase.getDatabase(application).userDao()
+        val repository = UserRepository(dao)
+        val factory = UserViewModelFactory(repository)
+
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+
+        binding.buttonSignUp.setOnClickListener{
+            val name = binding.editTextUsername.text.toString()
+            val email = binding.editTextEmail.text.toString()
+            val phoneNumber = binding.editTextPhone.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            val user = User(name = name, email = email, phoneNumber = phoneNumber, password = password)
+            viewModel.insert(user)
+
+            Toast.makeText(this, "Your account has been successfully created!", Toast.LENGTH_SHORT).show()
+        }
+
+        // Customized arrow
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+
+    }
+
+
+}
